d633591c1ce3cce3ae8e885b089dca07525966c2
99oblivius <99dark.oblivion99@gmail.com>
2024-06-13 21:08:06 +0000

    made it prettier

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,16 +3,11 @@ def format_duration(seconds):
         ('days', 86400),
         ('hours', 3600),
         ('minutes', 60),
-        ('seconds', 1),
-    )
-
+        ('seconds', 1))
     result = []
-
     for name, count in intervals:
         value = seconds // count
-        if value:
-            seconds -= value * count
-            if value == 1: name = name.rstrip('s')
-            result.append(f"{value} {name}")
-
+        seconds -= value * count
+        if value == 1: name = name.rstrip('s')
+        result.append(f"{value} {name}")
     return ' '.join(result) if result else "0 seconds"
