249584fa650279893cce4cd3de962b14e6530a2f
99oblivius <99dark.oblivion99@gmail.com>
2024-07-10 14:20:40 +0200

    GPL license updated to AGPL terms

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,20 +1,19 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# VALORS Match Making Bot is a discord based match making automation and management service #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# 
+# VALORS Match Making Bot - Discord based match making automation and management service
 # Copyright (C) 2024 99oblivius, <projects@oblivius.dev>
-# 
+#
+# This file is part of VALORS Match Making Bot.
+#
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from enum import Enum
