c65fe348b6bc508ea3bf1456a26f3536de29b6cb
99oblivius <99dark.oblivion99@gmail.com>
2024-08-11 08:54:25 +0000

    added license

--- src/cogs/help/cog.py
+++ src/cogs/help/cog.py
@@ -1,3 +1,21 @@
+# VALORS Match Making Bot - Discord based match making automation and management service
+# Copyright (C) 2024 99oblivius, <projects@oblivius.dev>
+#
+# This file is part of VALORS Match Making Bot.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import nextcord
 from nextcord.ext import commands
 from nextcord import Interaction, SlashOption
