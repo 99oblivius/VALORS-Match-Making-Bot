add66c0c293507f9b8f53053b65ed325645e842f
99oblivius <99dark.oblivion99@gmail.com>
2024-08-11 07:48:25 +0000

    implemented lock for match acccepts

--- src/views/match/accept.py
+++ src/views/match/accept.py
@@ -32,6 +32,7 @@ class AcceptView(nextcord.ui.View):
         self.bot = bot
         self.timeout = None
         self.done_event = done_event
+        self.lock = asyncio.Lock()
         self.accepted_players = []
     
     @nextcord.ui.button(
@@ -49,20 +50,20 @@ class AcceptView(nextcord.ui.View):
             await msg.delete()
             return
         
-        await self.bot.store.update(MMBotMatchPlayers, 
-            guild_id=interaction.guild.id, match_id=match.id, user_id=interaction.user.id, accepted=True)
-        log.debug(f"{interaction.user.display_name} accepted match {match.id}")
-        players = await self.bot.store.get_players(match.id)
-        
-        embed = interaction.message.embeds[0]
-        embed.set_field_at(0, name="Attendance", value=format_mm_attendance(players))
-        await interaction.message.edit(embed=embed)
-        msg = await interaction.response.send_message(
-            "You accepted the match!", ephemeral=True)
-        
-        self.accepted_players = await self.bot.store.get_accepted_players(match.id)
-        if len(self.accepted_players) == MATCH_PLAYER_COUNT:
-            self.done_event.set()
+        async with self.lock:
+            await self.bot.store.update(MMBotMatchPlayers, 
+                guild_id=interaction.guild.id, match_id=match.id, user_id=interaction.user.id, accepted=True)
+            log.debug(f"{interaction.user.display_name} accepted match {match.id}")
+            players = await self.bot.store.get_players(match.id)
+            
+            embed = interaction.message.embeds[0]
+            embed.set_field_at(0, name="Attendance", value=format_mm_attendance(players))
+            await interaction.message.edit(embed=embed)
+            msg = await interaction.response.send_message(
+                "You accepted the match!", ephemeral=True)
+            self.accepted_players = await self.bot.store.get_accepted_players(match.id)
+            if len(self.accepted_players) == MATCH_PLAYER_COUNT:
+                self.done_event.set()
         
         await asyncio.sleep(3)
         await msg.delete()
