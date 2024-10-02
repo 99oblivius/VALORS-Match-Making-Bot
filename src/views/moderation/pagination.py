d832835b25c3b55498175a28a8d5742fd0b71f25
99oblivius <99dark.oblivion99@gmail.com>
2024-10-02 03:28:48 +0000

    pagination limited to staff

--- src/views/moderation/pagination.py
+++ src/views/moderation/pagination.py
@@ -7,6 +7,15 @@ class PaginationView(nextcord.ui.View):
         self.current_page = current_page
         self.total_pages = total_pages
         self.update_button_states()
+    
+    async def is_staff(self, interaction: nextcord.Interaction) -> bool:
+        settings = await self.bot.store.get_settings(interaction.guild.id)
+        staff_role = interaction.guild.get_role(settings.mm_staff_role)
+        if staff_role in interaction.user.roles:
+            return True
+        await interaction.response.send_message("Reserved for staff", ephemeral=True)
+        return False
+
 
     def update_button_states(self):
         self.first_page.disabled = self.current_page == 1
@@ -16,16 +25,20 @@ class PaginationView(nextcord.ui.View):
 
     @nextcord.ui.button(label="<<", style=nextcord.ButtonStyle.grey)
     async def first_page(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if not self.is_staff(interaction): return
         await self.callback(interaction, page=1)
 
     @nextcord.ui.button(label="<", style=nextcord.ButtonStyle.grey)
     async def prev_page(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if not self.is_staff(interaction): return
         await self.callback(interaction, page=self.current_page - 1)
 
     @nextcord.ui.button(label=">", style=nextcord.ButtonStyle.grey)
     async def next_page(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if not self.is_staff(interaction): return
         await self.callback(interaction, page=self.current_page + 1)
 
     @nextcord.ui.button(label=">>", style=nextcord.ButtonStyle.grey)
     async def last_page(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+        if not self.is_staff(interaction): return
         await self.callback(interaction, page=self.total_pages)
