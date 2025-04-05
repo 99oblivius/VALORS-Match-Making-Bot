808a26d920cc24ed3373c3a6f6bb2d4c079071f4
99oblivius <99dark.oblivion99@gmail.com>
2025-04-05 20:11:48 +0000

    fixed live scores

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -407,7 +407,7 @@ async def generate_score_image(cache, guild: Guild, match: MMBotMatches, match_s
 def generate_score_text(guild: Guild, persistent_stats: Dict[int, Dict[str, Any]]):
     scores = "```ansi\n"
     header = "\u001b[1m CT             |   K/D/S  |  T             |   K/D/S  \u001b[0m\n"
-    scores += f"```ansi\n\u001b[1m{header}\u001b[0m\n{'─' * len(header)}\n"
+    scores += f"\u001b[1m{header}\u001b[0m\n{'─' * len(header)}\n"
     
     def name_formatted(user_id):
         member = guild.get_member(user_id)
@@ -418,7 +418,7 @@ def generate_score_text(guild: Guild, persistent_stats: Dict[int, Dict[str, Any]
     def kda_formatted(stats):
         return f"{stats['kills']}/{stats['deaths']}/{stats['score']}".rjust(8)
     
-    '\n'.join(
+    scores += '\n'.join(
         f" \u001b[1;34m{name_formatted(team_a['user_id'])} \u001b[0m- {kda_formatted(team_a)} | \u001b[1;31m{name_formatted(team_b['user_id'])} \u001b[0m- {kda_formatted(team_b)} "
         for team_a, team_b in zip(
             sorted([stats | {"user_id": user_id} for user_id, stats in persistent_stats.items() if stats['ct_start']], key=lambda x: x['score'], reverse=True), 
